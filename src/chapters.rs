2596ac782310ae637e5f10d468a58588987d2e30
Ueda Akihiro <ue1999.d@gmail.com>
2022-01-19 14:15:27 +0900

    add ch data_types

--- src/chapters.rs
+++ src/chapters.rs
@@ -1,2 +1,3 @@
 pub mod ch2_1;
 pub mod ch3_1;
+pub mod ch3_2;
--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,5 @@ fn main() {
   println!("main");
   chapters::ch2_1::guessing_game();
   chapters::ch3_1::var_and_mut();
+  chapters::ch3_2::data_types();
 }
